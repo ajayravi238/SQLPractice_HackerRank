9dd29446fe15719a4585d53618dd005715a92333
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-12-15 21:21:34 +0530

    Update and rename Employee Salaries to Employee Salaries

---
+++ Employee Salaries.sql
@@ -0,0 +1,6 @@
+-- Select name from employee table whose salary is greater than 2000 and months less than 10 order them by employee id in ascending order
+
+SELECT NAME
+FROM EMPLOYEE
+WHERE SALARY > 2000 AND MONTHS < 10
+ORDER BY EMPLOYEE_ID ASC;
